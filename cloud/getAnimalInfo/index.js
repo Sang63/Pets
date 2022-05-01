70ff69e11af8706be7aab03001cb0d26bd02b1f4
江户川 姗 <2780287233@qq.com>
2022-05-01 10:00:14 +0800

    关注与取关操作

--- cloud/getAnimalInfo/index.js
+++ cloud/getAnimalInfo/index.js
@@ -1,21 +1,31 @@
-// 云函数入口文件
 const cloud = require('wx-server-sdk')
 
 cloud.init({
-    env: cloud.DYNAMIC_CURRENT_ENV
+  env: cloud.DYNAMIC_CURRENT_ENV
 })
 
-const db=cloud.database()
+const db = cloud.database()
 
-// 云函数入口函数
 exports.main = async (event) => {
-    const {id}=event
+  // const {
+  //   animalId,
+  //   userId
+  // } = event
 
-    const animal=db.collection('animal')
+  const { animalId,userId }=event
 
-    const {data}=await animal.doc(id).get()
+  const animalInfo = await db.collection('animal').doc(animalId).get() // 宠物信息
 
-    return {
-        data
+  const userInfo = await db.collection('userInfo').doc(userId).get() // 用户信息
+
+  return {
+    // data: {
+    //   ...animalInfo.data,
+      // like: userInfo.data.likeAnimalIds && userInfo.data.likeAnimalIds.includes(animalInfo.data._id)
+    // }
+    data:{
+      ...animalInfo.data,
+      like: userInfo.data.likeAnimalIds && userInfo.data.likeAnimalIds.includes(animalInfo.data._id)
     }
+  }
 }
--- pages/animalInfo/index.js
+++ pages/animalInfo/index.js
@@ -1,38 +1,97 @@
 // pages/animalInfo/index.js
 Page({
     data: {
-        id:null,
-        animalInfo:{}
+        id: null,
+        animalInfo: {}
     },
     //获取宠物详情
-    async getAnimalInfo(){
-        const { result: { data } }=await wx.cloud.callFunction({
-            name:'getAnimalInfo',
-            data:{
-                id:this.data.id
-            }
-        })
+    async getAnimalInfo() {
+        const { userId,id }=this.data
+        let animalInfo = {}
+        if(userId){
+            //登录操作
+            const {
+                result: {
+                    data
+                }
+            } = await wx.cloud.callFunction({
+                name: 'getAnimalInfo',
+                data: {
+                    userId,
+                    animalId:id
+                }
+            })
+            animalInfo = data
+            // this.setData({
+            //     animalInfo:data
+            // })
+        }else{
+            //非登录操作
+            const { data } = await wx.cloud.database().collection('animal').doc(id).get()
+            animalInfo = data
+        }
+        console.log(animalInfo)
         this.setData({
-            animalInfo:data
+            animalInfo
         })
-        console.log(data)
     },
-    onLoad: function ({id}) {
+    onLoad({
+        id
+    }) {
+        const userInfo = wx.getStorageSync("userInfo")
+        console.log(userInfo)
         this.setData({
-            id:id
+            id: id,
+            userId: userInfo._id
         })
         this.getAnimalInfo()
         //显示转发按钮
         wx.showShareMenu({
-          menus: ['shareAppMessage', 'shareTimeline'],
+            menus: ['shareAppMessage', 'shareTimeline'],
         })
     },
     //当前页面分享时触发，定义分享内容
-    onShareAppMessage(){
-        const {varieties,age}=this.data.animalInfo
-        return{
-            title:varieties + '---'+age+'个月',
-            path:`/pages/animalInfo/index?id=${id}`
+    onShareAppMessage() {
+        const {
+            varieties,
+            age
+        } = this.data.animalInfo
+        return {
+            title: varieties + '---' + age + '个月',
+            path: `/pages/animalInfo/index?id=${id}`
         }
-    }
+    },
+    //返回首页
+    backHome() {
+        //关闭所有页面,返回首页
+        wx.reLaunch({
+            url: '/pages/home/index',
+        })
+    },
+    //关注/取关
+    async onLike() {
+        const {
+            userId,
+            id
+        } = this.data;
+        if (!userId) {
+            wx.showToast({
+                icon: 'none',
+                title: '请登录后重试',
+            })
+            return;
+        }
+        wx.showLoading({
+            title: '操作中...',
+        })
+        await wx.cloud.callFunction({
+            name: 'patchLike',
+            data: {
+                userId,
+                animalId: id
+            }
+        })
+        this.getAnimalInfo()
+        wx.hideLoading()
+    },
 })
